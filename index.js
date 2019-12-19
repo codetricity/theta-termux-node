b228f07c9b2497d83bbc3bc550c87bfd18306406
craig <craig@oppkey.com>
2019-12-19 16:43:52 +0000

    added media directory

--- index.js
+++ index.js
@@ -59,47 +59,6 @@ app.post("/takePicture", (req, res) => {
   });
 });
 
-// example of POST with payload and parameters
-
-app.post("/listFiles", (req, res) => {
-  request({
-    headers: {
-      'content-type': 'application/json;charset=utf-8'
-    },
-    url: "http://localhost:8080/osc/commands/execute",
-    method: "POST",
-    json: {
-      name: "camera.listFiles",
-      parameters: {
-        "fileType": "image",
-        "entryCount": 2,
-        "maxThumbSize": 0        
-      }
-
-    }
-  }, (error, response, body) => {
-    
-      const localImageUri = body.results.entries[0].fileUrl;
-      const imageUrl = new URL(localImageUri);
-      const imagePath =  imageUrl.pathname;
-      console.log("first image path = " + imagePath);
-      console.log(ipAddress)
-    let webPage = JSON.stringify(body, null, 2) ;
-
-
-      imageUrl.host = ipAddress;
-      imageUrl.port = '80';
-
-
-      res.render('imageList', {
-	  webPage: webPage,
-	  imageUrl: imageUrl,
-	  clientMode: clientMode
-      } );
-
-  });
-});
-
 // example of getting files from underlying OS
 // full-size images
 
