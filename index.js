3c032dac36b9f4387e35fc59c87ddd2afa2e168f
craig <craig@oppkey.com>
2019-12-20 16:11:00 +0000

    set views folder

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ if (ipAddress == "192.168.1.1") {
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get("/", (req, res)=> {
